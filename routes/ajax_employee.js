04a51b2b772a6191a52a887506c739bc15157352
Andy Youens <andy@formaserve.co.uk>
2020-02-18 17:13:30 +0000

    complete delete functionality

--- routes/ajax_employee.js
+++ routes/ajax_employee.js
@@ -11,31 +11,44 @@ router.post('/', function(req, res, next) {
   var isAjaxRequest = req.xhr
   console.log(`Have we got an Ajax Call? ${isAjaxRequest}`)
 
-  // get post vars
-  let emid = req.body.emid
-  let action = req.body.action
+  // Check if this is an Ajax call
+  if (isAjaxRequest) {
+    // get post vars
+    let action = req.body.action
 
-  // Check action
-  if (action == 'delete') {
-    console.log(`Deleting Employee ID: ${emid}`)
+    // Check action
+    if (action == 'delete') {
+      let emid = req.body.emid
+      console.log(`Deleting Employee ID: ${emid}`)
 
-    async function execDelete(emid) {
-      const connection = new Connection({ url: '*LOCAL' })
-      const statement = new Statement(connection)
+      async function execDelete(emid) {
+        const connection = new Connection({ url: '*LOCAL' })
+        const statement = new Statement(connection)
 
-      let sql = `delete from ${schema}.employee where emid = ${emid}`
+        let sql = `delete from ${schema}.employee where emid = ${emid}`
 
-      await statement.prepare(sql)
+        await statement.prepare(sql)
 
-      await statement.execute(sql)
+        await statement.execute(sql)
 
-      res.send('ok')
+        res.send('ok')
+      }
+
+      execDelete(emid).catch(error => {
+        console.error(`Unable to delete employee ${emid} - Error: ${error}`)
+        res.send('error')
+      })
     }
+  } else {
+    // setup error messages & display error page
+    console.error('Ajax Header called directly!')
+    let message = 'Ajax Header called directly!'
+    let error = []
+    error.status = 'Direct call not an AJAX call'
+    error.stack = 'You cannot directly call an AJAX function'
 
-    execDelete(emid).catch(error => {
-      console.error(`Unable to delete employee ${emid} - Error: ${error}`)
-      res.send('error')
-    })
+    // Output to browser
+    res.render('error', { message, error })
   }
 })
 
