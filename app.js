d9f7412609be2fb54012e52cad771f313ac49bc5
Andy Youens <andy@formaserve.co.uk>
2020-04-30 11:32:24 +0100

    Included Add functionality

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+/**
+ *  PowerWire Article on Node.js on IBM i
+ *
+ *  Author: Andy Youens FormaServe Systems Ltd
+ *  https://www.formaserve.co.uk
+ *
+ */
+
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 const session = require('client-sessions')
 
 // routing bits
-var indexRouter = require('./routes/signon')
-var employeeRouter = require('./routes/employee')
-var signonRouter = require('./routes/signon')
-var addRouter = require('./routes/add')
-var ajaxEmployeeRouter = require('./routes/ajax_employee')
+const indexRouter = require('./routes/signon')
+const employeeRouter = require('./routes/employee')
+const signonRouter = require('./routes/signon')
+const ajaxEmployeeRouter = require('./routes/ajax_employee')
 
-var app = express()
+const app = express()
 
 // Setup session stuff
 app.use(
@@ -23,7 +30,7 @@ app.use(
     activeDuration: 1000 * 60 * 5,
     httpOnly: true,
     secure: true,
-    ephemeral: true
+    ephemeral: true,
   })
 )
 
@@ -43,16 +50,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/employee', employeeRouter)
 app.use('/signon', signonRouter)
-app.use('/add', addRouter)
+app.use('/', signonRouter)
 app.use('/ajax_employee', ajaxEmployeeRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404))
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
--- routes/ajax_employee.js
+++ routes/ajax_employee.js
@@ -1,32 +1,33 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+
+const router = express.Router()
+const debug = require('debug')
 const { Connection, Statement, IN, NUMERIC, CHAR } = require('idb-pconnector')
+
 const schema = 'HRDATA'
 
 /* POST home page. */
-router.post('/', function(req, res, next) {
-  
+router.post('/', function (req, res, next) {
   console.log('--> Into ajax_employee post')
 
-  var isAjaxRequest = req.xhr
+  const isAjaxRequest = req.xhr
   console.log(`Have we got an Ajax Call? ${isAjaxRequest}`)
 
   // Check if this is an Ajax call
   if (isAjaxRequest) {
-    
-    // get post vars
-    let action = req.body.action
+    // get post consts
+    const { action } = req.body
 
     // Delete Functionality
     if (action === 'delete') {
-      let emid = req.body.emid
+      const { emid } = req.body
       console.log(`Deleting Employee ID: ${emid}`)
 
       async function execDelete(emid) {
         const connection = new Connection({ url: '*LOCAL' })
         const statement = new Statement(connection)
 
-        let sql = `delete from ${schema}.employee where emid = ${emid}`
+        const sql = `delete from ${schema}.employee where emid = ${emid}`
 
         await statement.prepare(sql)
 
@@ -35,7 +36,7 @@ router.post('/', function(req, res, next) {
         res.send('ok')
       }
 
-      execDelete(emid).catch(error => {
+      execDelete(emid).catch((error) => {
         console.error(`Unable to delete employee ${emid} - Error: ${error}`)
         res.send('error')
       })
@@ -43,29 +44,34 @@ router.post('/', function(req, res, next) {
 
     // Update Functionality
     if (action === 'update') {
-      let emid = req.body.emid
-      let emfir = req.body.emfir
-      let emsur = req.body.emsur
-      let emdep = req.body.emdep
-      let fullName = req.body.emfir.trim() + ' ' + req.body.emsur.trim()
+      const { emid } = req.body
+      const { emfir } = req.body
+      const { emsur } = req.body
+      const { emdep } = req.body
+      const fullName = `${req.body.emfir.trim()} ${req.body.emsur.trim()}`
 
       async function execUpdate(emid) {
         console.log(`Updating Employee ID: ${emid} ${fullName} - Dep: ${emdep}`)
         const connection = new Connection({ url: '*LOCAL' })
         const statement = new Statement(connection)
 
-        let sql = `update ${schema}.employee set emdep = ?, emsur = ?, emfir = ?  where emid = ?`
+        const sql = `update ${schema}.employee set emdep = ?, emsur = ?, emfir = ?  where emid = ?`
 
         await statement.prepare(sql)
 
-        await statement.bindParam([[`${emdep}`, IN, CHAR], [`${emsur}`, IN, CHAR], [`${emfir}`, IN, CHAR], [`${emid}`, IN, NUMERIC]])
+        await statement.bindParam([
+          [`${emdep}`, IN, CHAR],
+          [`${emsur}`, IN, CHAR],
+          [`${emfir}`, IN, CHAR],
+          [`${emid}`, IN, NUMERIC],
+        ])
 
         await statement.execute(sql)
 
         res.send('ok')
       }
 
-      execUpdate(emid).catch(error => {
+      execUpdate(emid).catch((error) => {
         console.error(`Unable to update employee ${emid} - Error: ${error}`)
         res.send('error')
       })
@@ -73,41 +79,42 @@ router.post('/', function(req, res, next) {
 
     // Add Functionality
     if (action === 'add') {
-
-      let emfir = req.body.emfir
-      let emsur = req.body.emsur
-      let emdep = req.body.emdep
-      let fullName = req.body.emfir.trim() + ' ' + req.body.emsur.trim()
+      const { emfir } = req.body
+      const { emsur } = req.body
+      const { emdep } = req.body
+      const fullName = `${req.body.emfir.trim()} ${req.body.emsur.trim()}`
 
       async function execAdd() {
-
         console.log(`Adding Employee: ${fullName} - Dep: ${emdep}`)
 
         const connection = new Connection({ url: '*LOCAL' })
         const statement = new Statement(connection)
 
-        let sql = `insert into ${schema}.employee (emsur, emfir, emdep) values(?,?,?)`
+        const sql = `insert into ${schema}.employee (emsur, emfir, emdep) values(?,?,?)`
 
         await statement.prepare(sql)
 
-        await statement.bindParam([[`${emsur}`, IN, CHAR], [`${emfir}`, IN, CHAR], [`${emdep}`, IN, CHAR]])
+        await statement.bindParam([
+          [`${emsur}`, IN, CHAR],
+          [`${emfir}`, IN, CHAR],
+          [`${emdep}`, IN, CHAR],
+        ])
 
         await statement.execute(sql)
 
         res.send('ok')
       }
 
-      execAdd().catch(error => {
+      execAdd().catch((error) => {
         console.error(`Unable to add employee - Error: ${error}`)
         res.send('error')
       })
     }
-
   } else {
     // setup error messages & display error page
     console.error('Ajax Header called directly!')
-    let message = 'Ajax Header called directly!'
-    let error = []
+    const message = 'Ajax Header called directly!'
+    const error = []
     error.status = 'Direct call not an AJAX call'
     error.stack = 'You cannot directly call an AJAX function'
 
